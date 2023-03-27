3729c914f98c45e2b7a3bfda8459918c87f5a9d6
TafariBeckford <beckfordtafari55@gmail.com>
2023-03-27 00:50:00 -0500

    Updated Tools section

--- cloudresume/script.js
+++ cloudresume/script.js
@@ -1,12 +1,4 @@
 
-
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-
     $('.navbar .menu li a').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
@@ -27,31 +19,10 @@
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Cloud Engineer", "Blogger"],
+        strings: ["Cloud Engineer","DevOps Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+ 
